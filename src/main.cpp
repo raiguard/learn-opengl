2b90db1cf32b2ac10ba466a250172f7b72494a30
Caleb Heuer <ch@raiguard.me>
2024-06-30 15:24:34 -0600

    Import a ton of different materials

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "camera.hpp"
+#include "material.hpp"
 #include "shader.hpp"
 #include <cassert>
 #include <chrono>
@@ -12,6 +13,7 @@
 #include <imgui_impl_opengl3.h>
 #include <iostream>
 #include <SDL.h>
+#include <map>
 #include <thread>
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
@@ -43,6 +45,33 @@ int main()
 
   gladLoadGLLoader(SDL_GL_GetProcAddress);
   std::cout << std::format("OpenGL version: {}", (const char*)(glGetString(GL_VERSION))) << std::endl;
+
+  std::map<std::string, Material> materials = {
+    {"emerald", Material(0.0215f, 0.1745f, 0.0215f, 0.07568f,  0.61424f,  0.07568f,  0.633f,  0.727811f, 0.633f,  0.6f)},
+    {"jade", Material(0.135f,  0.2225f, 0.1575f, 0.54f, 0.89f, 0.63f, 0.316228f, 0.316228f, 0.316228f, 0.1f)},
+    {"obsidian", Material(0.05375f,  0.05f, 0.06625f,  0.18275f,  0.17f, 0.22525f,  0.332741f, 0.328634f, 0.346435f, 0.3f)},
+    {"pearl", Material(0.25f, 0.20725f,  0.20725f,  1.0f,  0.829f,  0.829f,  0.296648f, 0.296648f, 0.296648f, 0.088f)},
+    {"ruby", Material(0.1745f, 0.01175f,  0.01175f,  0.61424f,  0.04136f,  0.04136f,  0.727811f, 0.626959f, 0.626959f, 0.6f)},
+    {"turquoise", Material(0.1f,  0.18725f,  0.1745f, 0.396f,  0.74151f,  0.69102f,  0.297254f, 0.30829f,  0.306678f, 0.1f)},
+    {"brass", Material(0.329412f, 0.223529f, 0.027451f, 0.780392f, 0.568627f, 0.113725f, 0.992157f, 0.941176f, 0.807843f, 0.21794872f)},
+    {"bronze", Material(0.2125f, 0.1275f, 0.054f,  0.714f,  0.4284f, 0.18144f,  0.393548f, 0.271906f, 0.166721f, 0.2f)},
+    {"chrome", Material(0.25f, 0.25f, 0.25f, 0.4f,  0.4f,  0.4f,  0.774597f, 0.774597f, 0.774597f, 0.6f)},
+    {"copper", Material(0.19125f,  0.0735f, 0.0225f, 0.7038f, 0.27048f,  0.0828f, 0.256777f, 0.137622f, 0.086014f, 0.1f)},
+    {"gold", Material(0.24725f,  0.1995f, 0.0745f, 0.75164f,  0.60648f,  0.22648f,  0.628281f, 0.555802f, 0.366065f, 0.4f)},
+    {"silver", Material(0.19225f,  0.19225f,  0.19225f,  0.50754f,  0.50754f,  0.50754f,  0.508273f, 0.508273f, 0.508273f, 0.4f)},
+    {"blackPlastic", Material(0.0f,  0.0f,  0.0f,  0.01f, 0.01f, 0.01f, 0.50f, 0.50f, 0.50f, .25f)},
+    {"cyanPlastic", Material(0.0f,  0.1f,  0.06f, 0.0f,  0.50980392f, 0.50980392f, 0.50196078f, 0.50196078f, 0.50196078f, .25f)},
+    {"greenPlastic", Material(0.0f,  0.0f,  0.0f,  0.1f,  0.35f, 0.1f,  0.45f, 0.55f, 0.45f, .25f)},
+    {"redPlastic", Material(0.0f,  0.0f,  0.0f,  0.5f,  0.0f,  0.0f,  0.7f,  0.6f,  0.6f,  .25f)},
+    {"whitePlastic", Material(0.0f,  0.0f,  0.0f,  0.55f, 0.55f, 0.55f, 0.70f, 0.70f, 0.70f, .25f)},
+    {"yellowPlastic", Material(0.0f,  0.0f,  0.0f,  0.5f,  0.5f,  0.0f,  0.60f, 0.60f, 0.50f, .25f)},
+    {"blackRubber", Material(0.02f, 0.02f, 0.02f, 0.01f, 0.01f, 0.01f, 0.4f,  0.4f,  0.4f,  .078125f)},
+    {"cyanRubber", Material(0.0f,  0.05f, 0.05f, 0.4f,  0.5f,  0.5f,  0.04f, 0.7f,  0.7f,  .078125f)},
+    {"greenRubber", Material(0.0f,  0.05f, 0.0f,  0.4f,  0.5f,  0.4f,  0.04f, 0.7f,  0.04f, .078125f)},
+    {"redRubber", Material(0.05f, 0.0f,  0.0f,  0.5f,  0.4f,  0.4f,  0.7f,  0.04f, 0.04f, .078125f)},
+    {"whiteRubber", Material(0.05f, 0.05f, 0.05f, 0.5f,  0.5f,  0.5f,  0.7f,  0.7f,  0.7f,  .078125f)},
+    {"yellowRubber", Material(0.05f, 0.05f, 0.0f,  0.5f,  0.5f,  0.4f,  0.7f,  0.7f,  0.04f, .078125f)},
+  };
 
   float vertices[] = {
     -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
@@ -128,11 +157,13 @@ int main()
 
   SDL_SetRelativeMouseMode(SDL_TRUE);
 
-  ImVec4 lightColor(1.0f, 1.0f, 1.0f, 1.0f); // White
-  ImVec4 cubeColor(1.0f, 0.5f, 0.31f, 1.0f); // Coral
+  ImVec4 lightColor(1.0f, 1.0f, 1.0f, 1.0f);
 
   SDL_Event event;
   bool quit = false;
+
+  Material& selectedMaterial = materials["greenRubber"];
+
   while (!quit)
   {
     using Clock = std::chrono::high_resolution_clock;
@@ -231,12 +262,12 @@ int main()
     // activate the shader and set uniforms
     lightingShader.use();
     // material
-    lightingShader.setVec3("material.ambient", cubeColor.x, cubeColor.y, cubeColor.z);
-    lightingShader.setVec3("material.diffuse", cubeColor.x, cubeColor.y, cubeColor.z);
-    lightingShader.setVec3("material.specular", 0.5f, 0.5f, 0.5f);
-    lightingShader.setFloat("material.shininess", 32.0f);
+    lightingShader.setVec3("material.ambient", selectedMaterial.ambient);
+    lightingShader.setVec3("material.diffuse", selectedMaterial.diffuse);
+    lightingShader.setVec3("material.specular", selectedMaterial.specular);
+    lightingShader.setFloat("material.shininess", selectedMaterial.shininess * 32.0f);
     // light
-    lightingShader.setVec3("light.ambient",  0.2f, 0.2f, 0.2f);
+    lightingShader.setVec3("light.ambient",  1.0f, 1.0f, 1.0f);
     lightingShader.setVec3("light.diffuse",  lightColor.x * 0.5f, lightColor.y * 0.5f, lightColor.z * 0.5f); // darken diffuse light a bit
     lightingShader.setVec3("light.specular", lightColor.x, lightColor.y, lightColor.z);
     lightingShader.setVec3("light.position", lightPos);
@@ -282,9 +313,19 @@ int main()
     if (ImGui::Checkbox("Use vsync", &useVsync))
       SDL_GL_SetSwapInterval(useVsync ? 1 : 0);
     ImGui::Checkbox("Rotate cube", &rotateCube);
-    ImGui::Text("Cube color:");
-    ImGui::SameLine();
-    ImGui::ColorEdit3("Cube color", (float*)&cubeColor, ImGuiColorEditFlags_NoInputs | ImGuiColorEditFlags_NoLabel);
+    // ImGui::Text("Cube color:");
+    // ImGui::SameLine();
+    // ImGui::ColorEdit3("Cube color", (float*)&cubeColor, ImGuiColorEditFlags_NoInputs | ImGuiColorEditFlags_NoLabel);
+    if (ImGui::Button("Cube material..."))
+      ImGui::OpenPopup("Select cube material");
+    if (ImGui::BeginPopup("Select cube material"))
+    {
+      for (auto& [name, material] : materials)
+        if (ImGui::Selectable(name.c_str()))
+          selectedMaterial = material;
+      ImGui::EndPopup();
+    }
+
     ImGui::Text("Light color:");
     ImGui::SameLine();
     ImGui::ColorEdit3("Light color", (float*)&lightColor, ImGuiColorEditFlags_NoInputs | ImGuiColorEditFlags_NoLabel);
